0161b6aab6b7106e99942fd76a7859910d5bed80
Graeme Maciver <graeme.maciver@cohaesus.co.uk>
2013-07-06 22:57:59 +0100

    contents and js

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      main: {
+        files: [
+          {expand: true, cwd: 'source/', src: ['*'], dest: 'build/', filter: 'isFile'},
+          {expand: true, cwd: 'source/', src: ['assets/**'], dest: 'build/'},
+        ]
+      }
+    },
+    sass: {
+      main: {
+        files: {
+          'build/assets/css/style.css': 'build/assets/scss/style.scss',
+        }
+      }
+    },    
+    swig: {
+      development: {
+        init: {
+          root: "source/",
+          allowErrors: false,
+          autoescape: true
+        },
+        dest: "build/",
+        cwd: "source/",
+        src: ['**/*.swig'],
+        siteUrl: 'http://mydomain.net/',
+        sitemap_priorities: {
+            '_DEFAULT_': '0.5',
+            'index': '0.8',
+        }
+      }
+    },
+    watch: {
+      files: ['source/**/*'],
+      tasks: ['copy', 'sass', 'swig']
+    }
+  });
+
+  // Load plugins
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-swig');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Add tasks
+  grunt.registerTask('default', ['copy', 'sass', 'swig']);
+
+};
