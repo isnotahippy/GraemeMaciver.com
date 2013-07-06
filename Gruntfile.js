8eca626cc18906335131df3e704eff2f7f0f70f4
Graeme Maciver <graeme.maciver@cohaesus.co.uk>
2013-07-07 00:49:38 +0100

    Updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,7 +50,7 @@ module.exports = function(grunt) {
       'gh-pages': {
         // These files will get pushed to the `gh-pages` branch (the default).
         base: 'build',
-        src: ['**/*']
+        src: ['*']
       },
     }
   });
